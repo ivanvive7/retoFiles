1e795664e2bcce17723be90e783c38548a8c3839
santimore <santimore999@gmail.com>
2026-01-30 10:17:04 +0100

    main actualizado

--- src/principal/ConcesionarioMain.java
+++ src/principal/ConcesionarioMain.java
@@ -15,48 +15,6 @@ import utilidades.MyObjectOutputStream;
 import utilidades.Utilidades;
 
 public class ConcesionarioMain {
-<<<<<<< HEAD
-	
-	public static int menu() {
-		int opcion;
-		System.out.println("--------------------------MENÚ--------------------------"
-				+ "\n1.  Introducir vehículos."
-				+ "\n2.  Listar vehículos."
-				+ "\n3.  Listar vehículos por tipo."
-				+ "\n4.  Modificar vehículo."
-				+ "\n5.  Eliminar un vehículo defectuoso."
-				+ "\n6.  Introducir clientes que van a reservar o comprar un vehículo."
-				+ "\n7.  Añadir una compra o reserva a un cliente ya registrado."
-				+ "\n8.  Mostrar clientes."
-				+ "\n9.  Buscar clientes."
-				+ "\n10. Salir."
-				+ "\nSeleccione una opción: ");
-		opcion=Utilidades.leerInt(1,10);
-		return opcion;
-	}
-	
-
-	
-	public static boolean validarMatricula(String matricula) throws MatriculaException{
-		boolean valido=false;
-		if(Pattern.matches("^[0-9]{4}[ ]?[BCDFGHJKLMNPQRSTVWXYZ]{3}$", matricula)) {
-			valido=true;
-		}else {
-			throw new MatriculaException("Error: el formato de la matrícula no es correcto."
-					+ "\nLa matrícula debe estar formada por cuatro números y tres letras (exluyendo vocales, Ñ y Q)");
-		}
-		return valido;
-	}
-	
-	public static boolean validarDni(String dni) throws DniException {
-		boolean valido=false;
-		if(Pattern.matches("^[0-9]{8}[A-HJ-NP-TV-Z]$", dni)) {
-			valido=true;
-		}else {
-			throw new DniException("Error: el formato del DNI no es correcto");
-		}
-		return valido;
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
