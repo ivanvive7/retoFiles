bfcc6e7cf269da0224a43ad9e43b27f3c8fbe96b
Etna2886 <etna2886gmail.com>
2026-01-30 11:41:18 +0100

    case2

--- src/principal/Case2.java
+++ src/principal/Case2.java
@@ -1,11 +1,35 @@
 package principal;
 
+import java.io.*;
+
+import clases.*;
+
 public class Case2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
+	public static void listarVehiculos(File fichV) {
+		ObjectInputStream ois=null;
+		boolean finArchivo=false;
+		if(fichV.exists()) {
+			try {
+				ois=new ObjectInputStream(new FileInputStream(fichV));
+				while(!finArchivo) {
+					try {
+						Vehiculo v=(Vehiculo) ois.readObject();
+						System.out.println(v);
+						}catch(EOFException v) {
+							finArchivo=true;
+					}
+				}
+			}catch (FileNotFoundException e) {
+				System.out.println("No se encontró el fichero.");
+			}catch (ClassNotFoundException e) {
+				System.out.println("La clase Vehículo no es válida.");
+			}catch (IOException e) {
+				System.out.println("Error leyendo el fichero.");
+			}
+		}else {
+			System.out.println("El fichero no existe.");
+		}
 	}
 
 }
