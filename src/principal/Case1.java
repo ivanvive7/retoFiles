8c1c030e28996aee03cc1085591ad6f4771b3e49
santimore <santimore999@gmail.com>
2026-01-30 12:26:20 +0100

    modificacion del case 1 no esta acabado

--- src/principal/Case1.java
+++ src/principal/Case1.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
-import clases.Vehiculo;
+import clases.*;
 import excepciones.MatriculaException;
 import utilidades.MyObjectOutputStream;
 import utilidades.Utilidades;
@@ -18,8 +18,11 @@ public class Case1 {
 		ObjectInputStream ois = null;
 		ObjectOutputStream oos = null;
 		MyObjectOutputStream moos = null;
-		boolean finArchivo = false, valida = false;
-		String matricula;
+		boolean finArchivo = false, valida = false, correcto = false;
+		String matricula="", marca ="", modelo = "", respuestaEstado = "";
+		double precioBase = 0.0;
+		Estado estado = null;
+		Combustible combustible = null;
 		
 		if (!fichV.exists()) {
 			try {
@@ -31,13 +34,14 @@ public class Case1 {
 
 				    try {
 				        valida = ConcesionarioMain.validarMatricula(matricula);
-				        System.out.println("Matrícula válida ✅");
+				        
 				    } catch (MatriculaException e) {
 				        System.out.println(e.getMessage());
 				    }
 
 				} while (!valida);
-					
+				System.out.println("Matrícula válida");
+			
 			} catch (FileNotFoundException e) {
 				System.out.println("Error, fichero no encontrado");
 			} catch (IOException e) {
@@ -45,6 +49,36 @@ public class Case1 {
 			}
 			
 			System.out.println("Introduce la marca del vehiculo: ");
+			marca = Utilidades.introducirCadena();
+			System.out.println("Introduce el modelo del vehiculo: ");
+			modelo = Utilidades.introducirCadena();
+			System.out.println("Introduce el precio base del vehiculo: ");
+			precioBase = Utilidades.leerDouble();
+			System.out.println("Introduce el estado del vehiculo (DISONIBLE | RESERVADO | VENDIDO): ");
+
+			while (!correcto) {
+			    try {
+			        respuestaEstado = Utilidades.introducirCadena();
+			        estado = Estado.valueOf(respuestaEstado.toUpperCase());
+			        correcto = true;
+			    } catch (IllegalArgumentException e) {
+			        System.out.println("El valor \"" + respuestaEstado + "\" no es válido. Inténtalo de nuevo.");
+			        System.out.println("Debe ser: DISPONIBLE, RESERVADO o VENDIDO");
+			    }
+			}
+			
+			System.out.println("Introduce el estado del vehiculo (GASOLINA | DIESEL | HIBRIDO | ELECTRICO): ");
+			
+			while (!correcto) {
+			    try {
+			        respuestaEstado = Utilidades.introducirCadena();
+			        estado = Estado.valueOf(respuestaEstado.toUpperCase());
+			        correcto = true;
+			    } catch (IllegalArgumentException e) {
+			        System.out.println("El valor \"" + respuestaEstado + "\" no es válido. Inténtalo de nuevo.");
+			        System.out.println("Debe ser: GASOLINA, DIESEL, HIBRIDO o ELECTRICO");
+			    }
+			}
 			
 		} else {
 			
